e5fbe9113eb81065a57e9ab09a7f2f1a53e8ccaf
MOHAMED IRFAN A <mohamedirfana.ug22.it@francisxavier.ac.in>
2026-06-23 17:45:24 +0530

    Fixed backend connection URLs

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE || 'https://nanban-backend.onrender.com/api'
+  baseURL: 'https://nanban-backend.onrender.com/api'
 })
 
 api.interceptors.request.use((config) => {
--- frontend/src/utils/axiosConfig.js
+++ frontend/src/utils/axiosConfig.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'https://nanban-backend.onrender.com/api',
+  baseURL: 'https://nanban-backend.onrender.com/api',
 });
 
 // Add a request interceptor to add the auth token
